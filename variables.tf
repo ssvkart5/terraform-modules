5946b75fd502f21325d4146c5cdd3e19b44a403a
ssvkart5 <61339095+ssvkart5@users.noreply.github.com>
2020-06-19 16:50:38 +0530

    Terraform-module codes
    Terraform-module codes

---
+++ variables.tf
@@ -0,0 +1,17 @@
+
+variable "vpc_cidr" {}
+variable "vpc_name" {}
+variable "IGW_name" {}
+variable "key_name" {}
+variable "public_subnet1_cidr" {}
+variable "public_subnet2_cidr" {}
+variable "public_subnet3_cidr" {}
+#variable "private_subnet_cidr" {}
+variable "public_subnet1_name" {}
+variable "public_subnet2_name" {}
+variable "public_subnet3_name" {}
+#variable "private_subnet_name" {}
+variable Main_Routing_Table {}
+
+
+
